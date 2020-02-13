be6c815df3340ed7913ab0ef3b7609179a5a2e90
Amy Stefani <amystefani1@gmail.com>
2020-02-12 21:59:25 -0800

    updating lesson reducer code to same format as modules

--- src/reducers/lessonReducer.js
+++ src/reducers/lessonReducer.js
@@ -1,35 +1,36 @@
-// const lessons = [
-//     {_id: "123", title: "Lesson 123"},
-//     {_id: "234", title: "Lesson 234"},
-//     {_id: "345", title: "Lesson 345"},
-//     {_id: "456", title: "Lesson 456"}
-// ]
-
-// ===> (State A)
+import {
+  CREATE_LESSON,
+  FIND_LESSONS_FOR_MODULE,
+  DELETE_LESSON,
+  FIND_LESSON,
+  UPDATE_LESSON
+} from "../actions/lessonActions";
 
 const lessonReducer = (state = { lessons: [] }, action) => {
   switch (action.type) {
-    case "CREATE_LESSON":
+    case CREATE_LESSON:
       return {
         lessons: [...state.lessons, action.lesson]
       };
-    case "DELETE_LESSON":
-      return {
-        lessons: state.lessons.filter(lesson => lesson._id !== action.lessonId)
-      };
-    case "UPDATE_LESSON":
-      return {
-        lessons: state.lessons.map(lesson =>
-          lesson._id === action.lessonId ? action.lesson : lesson
-        )
-      };
-    case "FIND_LESSONS_FOR_MODULE":
+    case FIND_LESSONS_FOR_MODULE:
       return {
         lessons: action.lessons
       };
-    case "FIND_ALL_LESSONS":
+    case DELETE_LESSON:
       return {
-        lessons: action.lessons
+        lessons: [...state.lessons]
+        // lessons: state.lessons.filter(lesson => lesson._id !== action.lessonId)
+      };
+    case FIND_LESSON:
+      return {
+        lesson: action.lesson
+      };
+    case UPDATE_LESSON:
+      return {
+        lesson: action.lesson
+        // lessons: state.lessons.map(lesson =>
+        //   lesson._id === action.lessonId ? action.lesson : lesson
+        // )
       };
     default:
       return state;
