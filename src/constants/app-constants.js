290291f80bc99b301632255602625df40c28a0f8
Amy Stefani <amystefani1@gmail.com>
2020-04-13 14:30:59 -0700

    updating api urls from heroku to local

--- src/constants/app-constants.js
+++ src/constants/app-constants.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://wbdv-generic-server.herokuapp.com/api/stefamy4405"
-export const LOCAL_API_URL = "http://localhost:8080/api"
-// export const LOCAL_API_URL ="https://fast-dawn-88684.herokuapp.com/api"
+// export const LOCAL_API_URL = "http://localhost:8080/api"
+export const LOCAL_API_URL ="https://fast-dawn-88684.herokuapp.com/api"
